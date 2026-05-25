5bf0e91ea1abf5db4fde210f5d2fa67424aa574f
vlab-dev-m2 <csapuras@gmail.com>
2026-05-25 12:46:27 +0800

    feat: working changing button state

--- src/components/Reports.tsx
+++ src/components/Reports.tsx
@@ -1,4 +1,5 @@
 import { actions } from 'astro:actions';
+import { useEffect, useState } from 'react';
 
 interface Data {
         lat: string;
@@ -14,12 +15,19 @@ interface Data {
         id:string;
     }
 
+interface StatusState {
+    id: string;
+    status: string;
+}
+
 export default function Reports ({data, token}:{data:Array<Data>, token :string}) {
-    
-    const resolveItem = (event:any, id:string) => {
-        event.preventDefault();
-        console.log(id)
-    }
+    const [statuses, setStatuses] = useState(Array<StatusState>);
+
+    useEffect(()=>{
+        const filteredData = data.map(({ id, status }) => ({ id, status }));
+        setStatuses(filteredData);
+    },[])
+
     return (
     <>
     <div className="max-h-100 overflow-x-auto rounded border border-gray-300 shadow-sm dark:border-gray-600">
@@ -39,8 +47,9 @@ export default function Reports ({data, token}:{data:Array<Data>, token :string}
             </thead>
             <tbody className="divide-y divide-gray-200 bg-white">
             { data.map((item)=>{
+                const currentStatus = statuses.filter((status)=>{return status?.id === item.id})[0]
                 return (
-                    <tr className="*:text-gray-900 *:first:font-medium" key={item.id}>
+                    <tr className="*:text-gray-900 *:first:font-medium" key={item.id} id={item.id}>
                         <td className="px-3 py-2 whitespace-nowrap">{new Intl.DateTimeFormat('en-US').format(new Date(item.created_at))}</td>
                         <td className="px-3 py-2 whitespace-nowrap">{item.name}</td>
                         <td className="px-3 py-2 whitespace-nowrap">{item.contact}</td>
@@ -48,19 +57,22 @@ export default function Reports ({data, token}:{data:Array<Data>, token :string}
                         <td className="px-3 py-2 whitespace-nowrap">{item.barangay}</td>
                         <td className="px-3 py-2 whitespace-nowrap">{item.address}</td>
                         <td className="max-w-50 px-3 py-2">{item.details}</td>
-                        <td className="px-3 py-2 whitespace-nowrap">{item.status}</td>
-                        { item.status === "pending" ? 
-                            <td>
-                            <a className="inline-flex items-center gap-2 rounded-sm border border-black-300 bg-yellow-300 px-2 py-1 text-yellow hover:bg-transparent hover:text-yellow-600" href="#"
-                                onClick={async () => {
-                                    const t = await actions.solve_report({id:item.id, token:token })
-                                    console.log(t)
-                                }}
-                            >
-                                <span className="text-sm font-small"> Solve </span>
-                            </a>
-                        </td> : <td></td>   
-                    }
+                        <td className="px-3 py-2 whitespace-nowrap">{currentStatus?.status}</td>
+                        { currentStatus?.status === "pending" ? 
+                                <td>
+                                <a className="inline-flex items-center gap-2 rounded-sm border border-black-300 bg-yellow-300 px-2 py-1 text-yellow hover:bg-transparent hover:text-yellow-600" href="#"
+                                    onClick={async () => {
+                                        const response = await actions.solve_report({id:item.id, token:token })
+                                        if(response?.data?.success){
+                                            currentStatus.status = 'done'
+                                            setStatuses([...statuses, currentStatus])
+                                        }
+                                    }}
+                                >
+                                    <span className="text-sm font-small"> Solve </span>
+                                </a>
+                            </td> : <td></td>   
+                        }
                     </tr>
                 )
             })}
