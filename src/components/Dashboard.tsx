1ead9a9c90869f5729a7573df6237ad0e3304eb4
vlab-dev-m2 <csapuras@gmail.com>
2026-06-02 13:05:30 +0800

    feat: revert to footer link

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,28 +4,13 @@ import { actions } from 'astro:actions';
 import { useEffect, useState } from 'react';
 import Reports from './Reports.js';
 
-export default function Dashboard ({ path }: { path: string }) {
+export default function Dashboard () {
     const $persistentAuthState = useStore(persistentAuthState); 
     console.log("Dashboard:", $persistentAuthState);
-    const hideLoginLink:boolean = (path === "/login");
-    const isLoggedIn  = $persistentAuthState.isLoggedIn;
-    const user = $persistentAuthState.username || "Unknown User";
-    
-    const logout = () => {
-        persistentAuthState.setKey('isLoggedIn', false);
-        persistentAuthState.setKey('token', "");
-        persistentAuthState.setKey('username', "");
-    }
-
-  const logClass = "text-2xl text-(--text-on-dominant) hover:border-b-1 hover:border-(--color-accent) my-10 p-2";
 
     return (
         <>
         <div>
-            <p className="py-5">{ hideLoginLink ? "" : isLoggedIn ?
-                <a onClick={logout} href="#" className={logClass}>Logout <span className="text-(--link-color)">@{user}</span></a> : 
-                <a href="/login" className={logClass}>Login</a>
-            }</p>
             <Reports />
         </div>
         </> 
--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,11 +1,29 @@
 import { useStore } from '@nanostores/react';
 import { persistentAuthState } from '../mainStore';
 
-export default function FooterComponent () {
-  
+export default function FooterComponent ({ path }: { path: string }) {
+    const $persistentAuthState = useStore(persistentAuthState); 
+    console.log("Dashboard:", $persistentAuthState);
+    const hideLoginLink:boolean = (path === "/login");
+    const isLoggedIn:boolean  = $persistentAuthState.isLoggedIn === 'true';
+    const user = $persistentAuthState.username || "Unknown User";
+    
+    const logout = () => {
+        persistentAuthState.setKey('isLoggedIn', false);
+        persistentAuthState.setKey('token', "");
+        persistentAuthState.setKey('username', "");
+    }
+
+  const logClass = "text-2xl text-(--text-on-dominant) hover:border-b-1 hover:border-(--color-accent) my-10 p-2";
   return (
   <> 
     <div className="footer-container p-2 text-center fixed inset-x-0 bottom-0 bg-(--color-dominant) border-t-1 border-solid border-(--color-secondary)">
+      <div>
+          <p className="py-5">{ hideLoginLink ? "" : isLoggedIn ?
+                <a onClick={logout} href="#" className={logClass}>Logout <span className="text-(--link-color)">@{user}</span></a> : 
+                <a href="/login" className={logClass}>Login</a>
+            }</p>
+      </div>
       <p className="text-sm text-(--text-on-dominant)">© 2026 Valentia Lab. All rights reserved.</p>
     </div>
   </>  
